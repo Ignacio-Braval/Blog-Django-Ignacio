70bddb6d96624445ae5a55fc52b924f864c95c71
ignacioblogdjango@gmail.com <ignaciocss@gmail.com>
2022-03-03 17:12:31 -0300

    Primera Version del Proyecto

--- categories/api/permissions.py
+++ categories/api/permissions.py
@@ -3,7 +3,6 @@ from rest_framework.permissions import BasePermission
 # Esta clase le permite solo a los admin ocupar el CRUD
 # de lo contrario solo podra ver los datos
 
-
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method == 'GET':
--- users/models.py
+++ users/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    #Se sobreescribe el metodo de login y ahora se pedira el email
-    #y no el username como se hace predeterminadamente
-    #agregar estas lineas luego de crear el super usuario en django
-    email = models.EmailField(unique=True)
+    # Se sobreescribe el metodo de login y ahora se pedira el email
+    # y no el username como se hace predeterminadamente
+    # agregar estas lineas luego de crear el super usuario en django
+     email = models.EmailField(unique=True)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+     USERNAME_FIELD = 'email'
+     REQUIRED_FIELDS = []
 
